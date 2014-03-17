ba4b23325b352a69492cb631317df3740171815a
Juank90 <juancamo90@hotmail.com>
2014-03-16 19:12:44 -0500

    JMC ,LAYO  & ESTEFANY
    agregamos estados

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,14 @@ angular.module('rutasApp', [
 		})
 		.state('usuarios', {
 			url: '/users',
-			template: '<h2>Modificación de usuarios</h2><a ui-sref="usuarios.listado">Ir al usuarios</a><div ui-view>Aqui el listado</div>'
+			template: '<h2>Modificación de usuarios</h2><a ui-sref="usuarios.listado">Ir al usuarios</a><div ui-view>Aqui el listado</div> <button  class="btn btn-navbar btn-primary"ui-sref="usuarios.preguntas">primera pregunta</button>'
+		})
+		.state('usuarios.preguntas', {
+			url: '/users',
+			template: ' <h1> RESPONDE:</h1><h2> prengunta: cual es la masa del sol</div>'
+		})
+		.state('usuarios.', {
+			url: '/users',
+			template: ' <h1> RESPONDE:</h1><h2> prengunta: cual es la masa del sol</div>'
 		});
-
 }])
